049a1200e6576f8047763b95f84b4d29549f8f6f
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2024-08-01 09:06:51 +0300

    Add files via upload

---
+++ Homework9boot/src/main/java/ua/meta/atipikin/Homework9bootApplication.java
@@ -0,0 +1,12 @@
+package ua.meta.atipikin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework9bootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework9bootApplication.class, args);
+	}
+}
---
+++ Homework9boot/src/test/java/ua/meta/atipikin/Homework9bootApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.meta.atipikin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework9bootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
